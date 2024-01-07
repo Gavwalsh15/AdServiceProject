eab7a51b9f65ae196ec72efde25dc47a2f637920
Gavin <G00402296@atu.ie>
2024-01-07 02:15:50 +0000

    Inital Commit

---
+++ src/main/java/ie/atu/projectadservice/ProjectAdServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAdServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/projectadservice/ProjectAdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
