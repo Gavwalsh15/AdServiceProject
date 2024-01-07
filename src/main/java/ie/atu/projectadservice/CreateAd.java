c584815979554d1f37e17347945347ae13e90697
Gavin <G00402296@atu.ie>
2024-01-07 02:40:09 +0000

    Basically all the ad Code TODO: Add in the Populate main screen

---
+++ src/main/java/ie/atu/projectadservice/CreateAd.java
@@ -0,0 +1,42 @@
+package ie.atu.projectadservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAd {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Seller email cannot be blank")
+    @Email(message = "Invalid email address")
+    private String sellerEmail;
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min = 1, max = 50, message = "Title must be between 1 and 50 characters")
+    private String title;
+
+    private String adImageLink;
+
+    private float price;
+
+    @Size(max = 300, message = "Max 300 Characters")
+    private String description;
+
+    private LocalDate createDate;
+}
